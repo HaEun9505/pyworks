ac588b989ab28ed4b0ef68ac6262c539b01fda5b
HaEun9505 <0104whgkdms@naver.com>
2021-12-02 12:49:53 +0900

    day11

---
+++ day11/web_flask/start_app.py
@@ -0,0 +1,27 @@
+#웹 서버 만들기
+from flask import Flask, render_template, request
+
+app = Flask(__name__)   #Flask 객체 app 생성
+
+@app.route('/')     #@ - 애니테이션 '/' 루트 검토 ip - 127.0.0.1:5000 -> host:port
+def index():
+    return render_template('index.html')    #html 파일을 index 페이지에 전송
+    # return "<h1>Hello~ Flask!!</h1>"
+
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':    #'POST'방식이라면
+        id = request.form['id']     #name 값 id를 가져옴
+        pwd = request.form['password']  #name 값 password를 가져옴
+        name = request.form['name']
+        return render_template('member_info.html', id=id, pwd=pwd, name=name)
+    else:
+        return render_template('register.html') #GET방식
+
+@app.route("/loop_index")
+def get_loopindex():
+    items = ['a', 'b', 'c', 'd']
+    return render_template("loop_index.html", items=items)
+
+app.run(debug=True) #debug=True, 서비스 하기전 개발 모드
